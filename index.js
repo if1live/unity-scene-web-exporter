b1fa9f28ecae17a31d842d14beb17e1c9fc991b3
Travis <libsora25@gmail.com>
2016-11-19 15:07:39 +0000

    Deploy to GitHub Pages: 75defaec6f7e29593584fa1f92fdee8547868c81

---
+++ index.js
@@ -0,0 +1,40 @@
+const ejs = require('ejs');
+const fs = require('fs');
+
+function writeExamplePage(data, output) {
+    const baseTemplateFile = './views/pages/layout.ejs';
+    ejs.renderFile(baseTemplateFile, data, {}, function(err, str) {
+        fs.writeFile(output, str, function(err) {
+            if(err) {
+                return console.error("cannot write file : " + output);
+            }
+        });      
+    });
+}
+
+writeExamplePage({
+    scenefile: './sample-threejs/materials/scene.json',
+}, './threejs-materials.html');
+writeExamplePage({
+    scenefile: './sample-threejs/models/scene.json',
+}, './threejs-models.html');
+writeExamplePage({
+    scenefile: './sample-threejs/script-variables/scene.json',
+}, './threejs-script-variables.html');
+writeExamplePage({
+    scenefile: './sample-threejs/simple-scene/scene.json',
+}, './threejs-simple-scene.html');
+writeExamplePage({
+    scenefile: './sample-threejs/5minlab/scene.json',
+}, './threejs-5minlab-scene.html');
+writeExamplePage({
+    scenefile: './sample-threejs/lightmap/scene.json',
+}, './threejs-lightmap-scene.html');
+
+function writeIndexPage() {
+    ejs.renderFile('./views/pages/index.ejs', {}, {}, function(err, str) {
+        fs.writeFile('./index.html', str, function(err) {
+        });
+    });
+}
+writeIndexPage();
